5a55d3bf9340d502f38e74273e84fb633bed04fb
Giovanni <giovannicriscuolo95@gmail.com>
2020-11-15 12:12:23 +0100

    Added DataSaver class and the data structure to save

---
+++ DrumBeater/Assets/Scripts/Data.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class Data
+{
+    // Is it the first time the player is playing?
+    private bool firstTime;
+
+    // List containing completed songs' id
+    private List<int> songIDList;
+    // List containing completed songs' records
+    private List<int[]> songRecords;
+
+    public Data(bool firstTime, Dictionary<int, int[]> records)
+    {
+        this.firstTime = firstTime;
+
+        foreach(int n in records.Keys)
+        {
+            songIDList.Add(n);
+        }
+
+        foreach (int[] array in records.Values)
+        {
+            songRecords.Add(array);
+        }
+    }
+}
---
+++ DrumBeater/Assets/Scripts/DataSaver.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using UnityEngine;
+
+public static class DataSaver
+{
+    /// <summary>
+    /// Save the data on the disk.
+    /// </summary>
+    /// <param name="dataToSave"> Data variables containing all the data to save on disk </param>
+    public static void save(Data dataToSave)
+    {
+        string jsonData = JsonUtility.ToJson(dataToSave);
+
+        StreamWriter sw = new StreamWriter(Application.persistentDataPath + "/SaveFile.txt");
+        sw.Write(jsonData);
+
+        sw.Close();
+    }
+
+    /// <summary>
+    /// Load the data from the disk and return them. Return null if there is no data.
+    /// </summary>
+    public static Data load()
+    {
+        if (File.Exists(Application.dataPath + "/SaveFile.txt"))
+        {
+            StreamReader sr = new StreamReader(Application.persistentDataPath + "/SaveFile.txt");
+            string jsonData = sr.ReadToEnd();
+
+            sr.Close();
+
+            return JsonUtility.FromJson<Data>(jsonData);
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Delete player's data.
+    /// </summary>
+    public static void deleteData()
+    {
+        if (File.Exists(Application.persistentDataPath + "/SaveFile.txt"))
+        {
+            File.Delete(Application.persistentDataPath + "/SaveFile.txt");
+#if UNITY_EDITOR
+            UnityEditor.AssetDatabase.Refresh();
+#endif
+        }
+    }
+}
