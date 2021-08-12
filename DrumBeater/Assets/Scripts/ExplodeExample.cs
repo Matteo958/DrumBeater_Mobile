2d6064105c623730e1aacf0a25a62bd3fc993771
Matteo <matteo.menghini1@studenti.unimi.it>
2021-08-12 23:56:13 +0200

    added power up that is activated by clapping hands; added power up that is activated by clenching one of the two fists; added ExplodeExample script to test these two power ups; added gestures to rotate the game base to the right or left

---
+++ DrumBeater/Assets/Scripts/ExplodeExample.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeExample : MonoBehaviour
+{
+    private Vector3 _startPos;
+    private Quaternion _startRot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPos = transform.localPosition;
+        _startRot = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Explode()
+    {
+        transform.GetComponent<Rigidbody>().AddForce(new Vector3(transform.position.x * Random.Range(-15, 15), transform.position.y * Random.Range(0, 4), transform.position.z * Random.Range(-10, 10)) * 5f);
+    }
+
+    public void ReturnToStartPos()
+    {
+        StartCoroutine(Return());
+    }
+
+    IEnumerator Return()
+    {
+        transform.rotation = _startRot;
+        float t = 0;
+        while (Mathf.Abs(Vector3.Distance(transform.localPosition, _startPos)) > 0.00001)
+        {
+            
+            transform.localPosition = Vector3.Lerp(transform.localPosition, _startPos, t);
+            t += Time.deltaTime * 0.05f;
+            yield return null;
+        }
+
+        
+        transform.localPosition = _startPos;
+    }
+}
--- DrumBeater/Assets/Scripts/Gestures.cs
+++ DrumBeater/Assets/Scripts/Gestures.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Gestures : MonoBehaviour
@@ -8,95 +7,322 @@ public class Gestures : MonoBehaviour
     [SerializeField] private Transform _palmLeft = default;
     [SerializeField] private Transform _cubeExample;
 
-    private float _palmFaceUpStartHeightL, _palmFaceUpStartHeightR;
+    [SerializeField] private GameObject _thumbLeftEnd = default;
+    [SerializeField] private GameObject _indexLeftEnd = default;
+    [SerializeField] private GameObject _middleLeftEnd = default;
+    [SerializeField] private GameObject _ringLeftEnd = default;
+    [SerializeField] private GameObject _pinkyLeftEnd = default;
 
-    public int _powerUp1Active, _powerUp2Active;
+    [SerializeField] private GameObject _thumbRightEnd = default;
+    [SerializeField] private GameObject _indexRightEnd = default;
+    [SerializeField] private GameObject _middleRightEnd = default;
+    [SerializeField] private GameObject _ringRightEnd = default;
+    [SerializeField] private GameObject _pinkyRightEnd = default;
+
+    [SerializeField] private float _dispForBaseRotation = default;
+
+    public int _powerUp1Active;
+
+    public bool _canActivatePower1, _canActivatePower2, _handOpenLeft, _handOpenRight, _fistLeft, _fistRight, _canRotateRight, _canRotateLeft, _rotationStarted;
+
+    private Vector3 _palmRightStartPos;
+    private Vector3 _palmLeftStartPos;
+
     // Start is called before the first frame update
     void Start()
     {
         _powerUp1Active = 0;
-        _powerUp2Active = 0;
+
+        _canActivatePower1 = _canActivatePower2 = false;
+        _handOpenLeft = _handOpenRight = false;
+        _fistLeft = _fistRight = false;
+        _canRotateLeft = _canRotateRight = false;
+        _rotationStarted = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if (Input.GetButtonDown("PowerUp1"))
+        {
+            _canActivatePower1 = true;
+        }else if (Input.GetButtonDown("PowerUp2"))
+        {
+            _canActivatePower2 = true;
+        }
+        else if (Input.GetButtonDown("RotateLeft"))
+        {
+            _canRotateLeft = true;
+        }
+        else if (Input.GetButtonDown("RotateRight"))
+        {
+            _canRotateRight = true;
+        }
+        
     }
-
-    public void ActivateHandsUp()
+    #region Clapping PowerUp
+    public void ActivateHandsLookingToEachOther()
     {
         _powerUp1Active = _powerUp1Active == 2? _powerUp1Active:_powerUp1Active+1;
 
-        if(_powerUp1Active == 2)
+        if(_powerUp1Active == 2 && _canActivatePower1)
         {
-            _palmFaceUpStartHeightL = _palmLeft.transform.localPosition.y;
-            _palmFaceUpStartHeightR = _palmRight.transform.localPosition.y;
-            StartCoroutine(CheckHandsUp());
+            StartCoroutine(CheckHandsClapping());
         }
     }
 
-    public void DeactivateHandsUp()
+    public void DeactivateHandsLookingToEachOther()
     {
         _powerUp1Active = _powerUp1Active == 0 ? _powerUp1Active : _powerUp1Active - 1;
     }
 
-    public void ActivateHandsDown()
-    {
-        _powerUp2Active = _powerUp2Active == 2 ? _powerUp2Active : _powerUp2Active + 1;
-
-        _palmFaceUpStartHeightL = _palmLeft.transform.localPosition.y;
-        _palmFaceUpStartHeightR = _palmRight.transform.localPosition.y;
-        StartCoroutine(CheckHandsDown());
-    }
-
-    public void DeactivateHandsDown()
-    {
-        _powerUp2Active = _powerUp2Active == 0 ? _powerUp2Active : _powerUp2Active - 1;
-    }
-
-    IEnumerator CheckHandsUp()
+    IEnumerator CheckHandsClapping()
     {
         while (_powerUp1Active == 2)
         {
-            UpdatePalmPosHandsUp(_palmFaceUpStartHeightL + 0.1f, _palmFaceUpStartHeightR + 0.1f);
-            
-            yield return new WaitForSeconds(0.01f);
-        }
-        
-    }
-
-    IEnumerator CheckHandsDown()
-    {
-        while (_powerUp2Active == 2)
-        {
-            UpdatePalmPosHandsDown(_palmFaceUpStartHeightL - 0.1f, _palmFaceUpStartHeightR - 0.1f);
+            UpdatePalmPosHandsClapping(0.02f);
 
             yield return new WaitForSeconds(0.01f);
         }
 
     }
 
-    private void UpdatePalmPosHandsUp(float destinationL, float destinationR)
+    private void UpdatePalmPosHandsClapping(float destination)
     {
-        
-        if((_palmLeft.transform.localPosition.y >= destinationL) && (_palmRight.transform.localPosition.y >= destinationR))
+
+        if (Vector3.Distance(_palmLeft.localPosition, _palmRight.localPosition) < destination)
         {
-            _powerUp1Active = 0;
-            _cubeExample.GetComponent<Rigidbody>().useGravity = false;
-            _cubeExample.GetComponent<Rigidbody>().AddForce(_cubeExample.transform.up * 15);
+            _canActivatePower1 = false;
+            foreach (Transform cube in _cubeExample.transform)
+            {
+                cube.GetComponent<ExplodeExample>().Explode();
+            }
             //TO-DO: Power Up Hands Up
         }
     }
 
-    private void UpdatePalmPosHandsDown(float destinationL, float destinationR)
-    {
+    #endregion
 
-        if ((_palmLeft.transform.localPosition.y <= destinationL) && (_palmRight.transform.localPosition.y <= destinationR))
+    #region Left Fist
+    public void ActivateFistLeft()
+    {
+        _fistLeft = true;
+
+        if(_canActivatePower2)
+            StartCoroutine(CheckHandOpenLeft());
+    }
+
+    public void DeactivateFistLeft()
+    {
+        
+        _handOpenLeft = false;
+        _fistLeft = false;
+    }
+
+    IEnumerator CheckHandOpenLeft()
+    {
+        while (!_handOpenLeft && _fistLeft)
         {
-            _powerUp2Active = 0;
-            _cubeExample.GetComponent<Rigidbody>().useGravity = true;
-            //TO-DO: Power Up Hands Down
+            UpdateHandOpenLeft();
+
+            yield return new WaitForSeconds(0.01f);
+        }
+        Debug.Log("LeftHand Open");
+    }
+
+    private void UpdateHandOpenLeft()
+    {
+        if ((Vector3.Distance(_palmLeft.position, _thumbLeftEnd.transform.position) > 0.07f
+            && Vector3.Distance(_palmLeft.position, _indexLeftEnd.transform.position) > 0.06f
+            && Vector3.Distance(_palmLeft.position, _middleLeftEnd.transform.position) > 0.06f
+            && Vector3.Distance(_palmLeft.position, _ringLeftEnd.transform.position) > 0.06f
+            && Vector3.Distance(_palmLeft.position, _pinkyLeftEnd.transform.position) > 0.06f))
+        {
+            _handOpenLeft = true;
+            StartCoroutine(CheckFistLeft());
         }
     }
+
+    IEnumerator CheckFistLeft()
+    {
+        while (_canActivatePower2 && _fistLeft)
+        {
+            UpdatePalmPosFistLeft();
+            yield return new WaitForSeconds(0.01f);
+        }
+        Debug.Log("Left Fist");
+    }
+
+    private void UpdatePalmPosFistLeft()
+    {
+        if ((Vector3.Distance(_palmLeft.position, _thumbLeftEnd.transform.position) < 0.06f
+            && Vector3.Distance(_palmLeft.position, _indexLeftEnd.transform.position) < 0.05f
+            && Vector3.Distance(_palmLeft.position, _middleLeftEnd.transform.position) < 0.05f
+            && Vector3.Distance(_palmLeft.position, _ringLeftEnd.transform.position) < 0.05f
+            && Vector3.Distance(_palmLeft.position, _pinkyLeftEnd.transform.position) < 0.05f))
+        {
+            Debug.Log("Power Activated by Left Hand");
+            _handOpenLeft = false;
+
+            _canActivatePower2 = false;
+            foreach (Transform cube in _cubeExample.transform)
+            {
+                cube.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                cube.GetComponent<Collider>().enabled = false;
+                cube.GetComponent<ExplodeExample>().ReturnToStartPos();
+            }
+        }
+    }
+    #endregion
+
+    #region Right Fist
+    public void ActivateFistRight()
+    {
+       
+        _fistRight = true;
+
+        if (_canActivatePower2)
+            StartCoroutine(CheckHandOpenRight());
+    }
+
+    public void DeactivateFistRight()
+    {
+        
+        _handOpenRight = false;
+        _fistRight = false;
+    }
+
+    IEnumerator CheckHandOpenRight()
+    {
+        while (!_handOpenRight && _fistRight)
+        {
+            UpdateHandOpenRight();
+
+            yield return new WaitForSeconds(0.01f);
+        }
+        Debug.Log("RightHand Open");
+    }
+
+    private void UpdateHandOpenRight()
+    {
+        if ((Vector3.Distance(_palmRight.position, _thumbRightEnd.transform.position) > 0.07f
+            && Vector3.Distance(_palmRight.position, _indexRightEnd.transform.position) > 0.06f
+            && Vector3.Distance(_palmRight.position, _middleRightEnd.transform.position) > 0.06f
+            && Vector3.Distance(_palmRight.position, _ringRightEnd.transform.position) > 0.06f
+            && Vector3.Distance(_palmRight.position, _pinkyRightEnd.transform.position) > 0.06f))
+        {
+            _handOpenRight = true;
+            StartCoroutine(CheckFistRight());
+        }
+    }
+
+    IEnumerator CheckFistRight()
+    {
+        while (_canActivatePower2 && _fistRight)
+        {
+            UpdatePalmPosFistRight();
+            yield return new WaitForSeconds(0.01f);
+        }
+        Debug.Log("Right Fist");
+    }
+
+    private void UpdatePalmPosFistRight()
+    {
+
+
+        if ((Vector3.Distance(_palmRight.position, _thumbRightEnd.transform.position) < 0.06f
+            && Vector3.Distance(_palmRight.position, _indexRightEnd.transform.position) < 0.05f
+            && Vector3.Distance(_palmRight.position, _middleRightEnd.transform.position) < 0.05f
+            && Vector3.Distance(_palmRight.position, _ringRightEnd.transform.position) < 0.05f
+            && Vector3.Distance(_palmRight.position, _pinkyRightEnd.transform.position) < 0.05f))
+        {
+            Debug.Log("Power Activated by Right Hand");
+            _handOpenRight = false;
+
+            _canActivatePower2 = false;
+            foreach (Transform cube in _cubeExample.transform)
+            {
+                cube.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                cube.GetComponent<Collider>().enabled = false;
+                cube.GetComponent<ExplodeExample>().ReturnToStartPos();
+            }
+        }
+    }
+
+    #endregion
+
+    #region Left Rotation
+
+    public void ActivateRotationLeft()
+    {
+        _palmRightStartPos = _palmRight.transform.localPosition;
+        _rotationStarted = true;
+        if (_canRotateLeft)
+        {
+            StartCoroutine(RotateLeft());
+        }
+    }
+
+    public void DeactivateRotationLeft()
+    {
+        _rotationStarted = false;
+    }
+
+    IEnumerator RotateLeft()
+    {
+        while (_rotationStarted && _canRotateLeft)
+        {
+            UpdatePalmPosRightToLeft(_dispForBaseRotation);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    private void UpdatePalmPosRightToLeft(float displacement)
+    {
+
+        if (_palmRight.transform.localPosition.x < _palmRightStartPos.x - displacement)
+        {
+            _canRotateLeft = false;
+            Debug.Log("Left rotation");
+            // Rotate to Left
+        }
+    }
+    #endregion
+
+    #region Right Rotation
+    public void ActivateRotationRight()
+    {
+        _palmLeftStartPos = _palmLeft.transform.localPosition;
+        _rotationStarted = true;
+        if (_canRotateRight)
+        {
+            StartCoroutine(RotateRight());
+        }
+    }
+
+    public void DeactivateRotationRight()
+    {
+        _rotationStarted = false;
+    }
+    
+    IEnumerator RotateRight()
+    {
+        while (_rotationStarted && _canRotateRight)
+        {
+            UpdatePalmPosLeftToRight(_dispForBaseRotation);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    private void UpdatePalmPosLeftToRight(float displacement)
+    {
+        
+        if (_palmLeft.transform.localPosition.x > _palmLeftStartPos.x + displacement)
+        {
+            _canRotateRight = false;
+            Debug.Log("Right rotation");
+            // Rotate to Left
+        }
+    }
+    #endregion
 }
